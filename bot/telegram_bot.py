0c9b3750f78d07c10893abefc1bb95d4ef78476d
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-07-10 14:56:21 +0300

    Update telegram_bot.py

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -42,9 +42,10 @@ class ChatGPTTelegramBot:
         self.commands = [
             BotCommand(command='help', description=localized_text('help_description', bot_language)),
             BotCommand(command='reset', description=localized_text('reset_description', bot_language)),
+            BotCommand(command='analyze', description=localized_text('analyze_description', bot_language)),
             BotCommand(command='stats', description=localized_text('stats_description', bot_language)),
             BotCommand(command='resend', description=localized_text('resend_description', bot_language)),
-            BotCommand(command='analyze', description=localized_text('analyze_description', bot_language)),
+            
         ]
         # If imaging is enabled, add the "image" command to the list
         if self.config.get('enable_image_generation', False):
